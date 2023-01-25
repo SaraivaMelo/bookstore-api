3d67437035854db3fd621340ec7effa94d414711
SaraivaMelo <dfmelosaraiva@gmail.com>
2023-01-24 21:19:31 -0300

    Finalizando as classesde dominio

--- src/main/java/com/davi/bookstore/domain/Categoria.java
+++ src/main/java/com/davi/bookstore/domain/Categoria.java
@@ -1,15 +1,28 @@
 package com.davi.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-public class Categoria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+/*Ao aplicar essa nomenclatura abaixo utilizando o javax, será indicado que precisa criar uma tabela na base*/
+@Entity
+public class Categoria  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 
 	public Categoria() {
